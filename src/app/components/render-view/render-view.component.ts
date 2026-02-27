60d1f3da0e743d6db15747c7d81aa07043dbfc88
Alexander Nilsson <alexvnilsson@outlook.com>
2026-02-27 14:38:08 +0100

    refactor render backbone to svg

--- src/app/components/render-view/render-view.component.ts
+++ src/app/components/render-view/render-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, ElementRef, inject } from "@angular/core";
+import { afterEveryRender, afterNextRender, Component, effect, ElementRef, inject } from "@angular/core";
 import { ElementsService } from "../../services/elements.service";
 import { RenderingService } from "../../services/rendering.service";
 
@@ -15,15 +15,22 @@ export class RenderViewComponent {
     elements = this.elementService.elements;
 
     constructor() {
-        effect(() => {
-
-        },
-        { debugName: "elements effect" });
+        
     }
 
-    ngOnInit() {
-        this.rendering.setupCanvas(this.elementRef.nativeElement);
+    protected getCanvas(): HTMLCanvasElement {
+        const canvas = this.elementRef.nativeElement.querySelector("svg#renderView");
+        if (canvas == undefined) {
+            throw new Error("Canvas not found.");
+        }
+        return canvas;
+    }
 
-        
+    protected getCanvasContainer(): HTMLElement {
+        const container = this.elementRef.nativeElement.querySelector("div#renderViewContainer");
+        if (container == undefined) {
+            throw new Error("Canvas Container not found.");
+        }
+        return container;
     }
 }
