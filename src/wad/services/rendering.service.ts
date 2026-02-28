ec55b2a3580a0ab76abc63553cc8ea07aa52ff38
Alexander Nilsson <alexvnilsson@outlook.com>
2026-02-28 08:05:11 +0100

    add coordinate translation

--- src/wad/services/rendering.service.ts
+++ src/wad/services/rendering.service.ts
@@ -1,10 +1,25 @@
 import { ElementRef, inject, Injectable, Signal, signal } from "@angular/core";
+import { Coordinate } from "../types/coordinate";
 
 @Injectable({ providedIn: "root" })
 export class RenderingService {
     private _height = signal(0);   
 
     height = this._height.asReadonly();
+
+    translateCoordinate(coordinate: Coordinate, target: "canvasian" | "cartesian" = "canvasian"): Coordinate {
+        return { x: coordinate.x, y: this.translateYCoordinate(coordinate.y, target) };
+    }
+
+    translateYCoordinate(y: number, target: "canvasian" | "cartesian" = "canvasian"): number {
+        if (target === "canvasian") {
+            return -(y - this.height());
+        } else if (target === "cartesian") {
+            return (this.height() - y);
+        } else {
+            throw new Error(`Invalid translation target: ${target}`);
+        }
+    }
 
     setHeight(height: number) {
         this._height.update(h => h = height);
