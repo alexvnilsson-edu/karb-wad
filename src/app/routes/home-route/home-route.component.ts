aaed6069fc634c13d04d3b133212ca9c6fe96afd
Alexander Nilsson <alexvnilsson@outlook.com>
2026-02-28 15:17:07 +0100

    remove old home-route

--- src/app/routes/home-route/home-route.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "app-route-home",
-    templateUrl: "./home-route.component.html"
-})
-export class HomeRouteComponent {
-    
-}
