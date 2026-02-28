cee5c9195e6cb666cd20c5a01853a5d36c530008
Alexander Nilsson <alexvnilsson@outlook.com>
2026-02-28 15:16:57 +0100

    refactor app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, ActivationEnd, RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 import { WadModule } from '../wad/wad.module';
 import { ElementsService } from '../wad/services/elements.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WadModule],
+  imports: [RouterOutlet, WadModule, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -16,6 +16,7 @@ export class App {
   title = signal("TITLE");
 
   constructor() {
+    this.activatedRoute.title.subscribe(t => console.debug(t));
     this.activatedRoute.title.subscribe(title => this.title.set(title || "TITLE NULL"));
   }
 }
