79565972628a6aee44f1bf55e98f9c550b8dda21
Alexander Nilsson <alexvnilsson@outlook.com>
2026-03-04 11:32:49 +0100

    wip: register commands

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,10 @@ import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, ActivationEnd, RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 import { WadModule } from './wad/wad.module';
 import { ElementStorageService } from './wad/elements/element-storage.service';
+import { WadCommandService } from './wad/cli/commands/command.service';
+import { WadCommand } from './wad/cli/commands/command';
+import { WadCoordinatesTransformer } from './wad/cli/commands/transformers/coordinates.transformer';
+import { WadNumberTransformer } from './wad/cli/commands/transformers/number.transformer';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +14,27 @@ import { ElementStorageService } from './wad/elements/element-storage.service';
   styleUrl: './app.css'
 })
 export class App {
-  private elementService = inject(ElementStorageService)
+  private elementService = inject(ElementStorageService);
+  private commandService = inject(WadCommandService);
 
   private activatedRoute = inject(ActivatedRoute);
   title = signal("TITLE");
 
   constructor() {
     this.activatedRoute.title.subscribe(title => this.title.set(title || "TITLE NULL"));
+
+
+    this.commandService.registerTransformer("coordinates", new WadCoordinatesTransformer());
+    this.commandService.registerTransformer("number", new WadNumberTransformer());
+
+    this.registerRectangleCommand();
+  }
+
+  private registerRectangleCommand() {
+    const command = new WadCommand("rectangle", new Set("rect"));
+    command.addArgument("origin", "coordinates");
+    command.addArgument("width", "number");
+    command.addArgument("height", "number");
+    this.commandService.registerCommand(command);
   }
 }
