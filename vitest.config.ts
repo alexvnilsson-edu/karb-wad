c59a9bac28beaa181469fddff19274aa93eb29ab
Alexander Nilsson <alexvnilsson@outlook.com>
2026-03-04 22:57:42 +0100

    fix vitest path resolve

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
 // Learn more about Vitest configuration options at https://vitest.dev/config/
 
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true
   },
+  resolve: {
+    alias: {
+      "app": path.resolve(__dirname, "src/app")
+    }
+  }
 });
