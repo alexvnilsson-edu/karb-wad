f752db049d128e217b2d805570157e6cbd4c0bf2
Alexander Nilsson <alexvnilsson@outlook.com>
2026-03-04 11:47:32 +0100

    refactor command registration

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,7 @@ import { WadCommandService } from './wad/cli/commands/command.service';
 import { WadCommand } from './wad/cli/commands/command';
 import { WadCoordinatesTransformer } from './wad/cli/commands/transformers/coordinates.transformer';
 import { WadNumberTransformer } from './wad/cli/commands/transformers/number.transformer';
+import { RectangleWadModel } from './wad/elements/rectangle';
 
 @Component({
   selector: 'app-root',
@@ -14,27 +15,14 @@ import { WadNumberTransformer } from './wad/cli/commands/transformers/number.tra
   styleUrl: './app.css'
 })
 export class App {
-  private elementService = inject(ElementStorageService);
-  private commandService = inject(WadCommandService);
+  
 
   private activatedRoute = inject(ActivatedRoute);
   title = signal("TITLE");
 
   constructor() {
     this.activatedRoute.title.subscribe(title => this.title.set(title || "TITLE NULL"));
-
-
-    this.commandService.registerTransformer("coordinates", new WadCoordinatesTransformer());
-    this.commandService.registerTransformer("number", new WadNumberTransformer());
-
-    this.registerRectangleCommand();
   }
 
-  private registerRectangleCommand() {
-    const command = new WadCommand("rectangle", new Set("rect"));
-    command.addArgument("origin", "coordinates");
-    command.addArgument("width", "number");
-    command.addArgument("height", "number");
-    this.commandService.registerCommand(command);
-  }
+  
 }
--- src/app/routing/home/home-route.component.ts
+++ src/app/routing/home/home-route.component.ts
@@ -1,6 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { RenderViewComponent } from "../../rendering/render-view/render-view.component";
 import { WadModule } from "app/wad/wad.module";
+import { WadCommand } from "app/wad/cli/commands/command";
+import { RectangleWadModel } from "app/wad/elements/rectangle";
+import { ElementStorageService } from "app/wad/elements/element-storage.service";
+import { WadCommandService } from "app/wad/cli/commands/command.service";
+import { WadCoordinatesTransformer } from "app/wad/cli/commands/transformers/coordinates.transformer";
+import { WadNumberTransformer } from "app/wad/cli/commands/transformers/number.transformer";
 
 @Component({
     selector: "app-route-home",
@@ -12,5 +18,40 @@ import { WadModule } from "app/wad/wad.module";
     imports: [WadModule, RenderViewComponent]
 })
 export class HomeRouteComponent {
-    
+    private elementService = inject(ElementStorageService);
+    private commandService = inject(WadCommandService);
+
+    ngOnInit() {
+        this.registerCommandTransformers();
+        this.registerRectangleCommand();
+    }
+
+    private registerCommandTransformers() { 
+        this.commandService.registerTransformer(
+            "coordinates", 
+            new WadCoordinatesTransformer()
+        );
+        this.commandService.registerTransformer(
+            "number",
+            new WadNumberTransformer()
+        );
+    }
+
+  private registerRectangleCommand() {
+    const command = new WadCommand("rectangle", new Set("rect"));
+    command.addArgument("origin", "coordinates");
+    command.addArgument("width", "number");
+    command.addArgument("height", "number");
+    command.executor = (command) => {
+      const origin = command.arguments.get("origin");
+      const [x, y] = origin!.value;
+      const width = command.arguments.get("width")?.value;
+      const height = command.arguments.get("height")?.value;
+      const element = new RectangleWadModel(x, y, width, height);
+      console.debug(`adding element`, element);
+      this.elementService.add(element);
+      return true;
+    }
+    this.commandService.registerCommand(command);
+  }
 }
