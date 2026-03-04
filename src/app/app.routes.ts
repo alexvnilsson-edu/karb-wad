87947be9406ad4f42a61292ebd8878a557ce53c1
Alexander Nilsson <alexvnilsson@outlook.com>
2026-03-04 10:01:22 +0100

    fix app router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeRouteComponent } from './routing/home/home-route.component';
-import { TestRouteComponent } from './routing/test/test-route.component';
 
 export const routes: Routes = [
     {
