d9d6efdee1b357e65c3478c1638aa7c085414420
Alexander Nilsson <alexvnilsson@outlook.com>
2026-03-04 20:05:41 +0100

    remove app title strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, TitleStrategy } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { AppTitleStrategy } from './routing/app-title-strategy.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
-    { provide: TitleStrategy, useClass: AppTitleStrategy }
+    provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,28 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
-import { ActivatedRoute, ActivationEnd, RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { WadModule } from './wad/wad.module';
-import { ElementStorageService } from './wad/elements/element-storage.service';
-import { WadCommandService } from './wad/cli/commands/command.service';
-import { WadCommand } from './wad/cli/commands/command';
-import { WadCoordinatesTransformer } from './wad/cli/commands/transformers/coordinates.transformer';
-import { WadNumberTransformer } from './wad/cli/commands/transformers/number.transformer';
-import { RectangleWadModel } from './wad/elements/rectangle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WadModule, RouterLinkWithHref, RouterLinkActive],
+  imports: [RouterOutlet, WadModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  
-
-  private activatedRoute = inject(ActivatedRoute);
-  title = signal("TITLE");
-
-  constructor() {
-    this.activatedRoute.title.subscribe(title => this.title.set(title || "TITLE NULL"));
-  }
-
-  
-}
+export class App { }
