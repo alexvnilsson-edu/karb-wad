a374f9b655f6f20dfaf3fd2f599fb556f95e2031
Alexander Nilsson <alexvnilsson@outlook.com>
2026-03-15 16:54:55 +0100

    add keyboard interactivity flow to command

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, viewChild } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RenderViewComponent } from './rendering/render-view/render-view.component';
+import { CommandInput } from './wad/cli/command-input/command-input';
 import { configureCommands } from './wad/cli/commands/command-config';
 import { WadCommandService } from './wad/cli/commands/command.service';
 import { WadElementService } from './wad/rendering/element.service';
@@ -11,12 +12,25 @@ import { WadModule } from './wad/wad.module';
   imports: [WadModule, MatToolbarModule, RenderViewComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  host: {
+    '(window:keyup.enter)': 'keyupEnter()',
+  },
 })
 export class App {
   commandService = inject(WadCommandService);
   elementService = inject(WadElementService);
 
+  commandInput = viewChild(CommandInput);
+
   constructor() {
     configureCommands(this.commandService, this.elementService);
+  }
+
+  keyupEnter() {
+    if (this.commandInput()) {
+      if (!this.commandInput()!.inFocus$()) {
+        this.commandInput()!.setFocus();
+      }
+    }
   }
 }
