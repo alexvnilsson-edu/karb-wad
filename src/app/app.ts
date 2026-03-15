825cc6f4e155448a299461f4ca4a67be8063f629
Alexander Nilsson <alexvnilsson@outlook.com>
2026-03-15 15:18:09 +0100

    wip: material design

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RenderViewComponent } from './rendering/render-view/render-view.component';
 import { WadModule } from './wad/wad.module';
 
 @Component({
   selector: 'app-root',
-  imports: [WadModule, RenderViewComponent],
+  imports: [WadModule, MatToolbarModule, RenderViewComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/rendering/render-view/render-view-sidebar/render-view-sidebar.component.ts
+++ src/app/rendering/render-view/render-view-sidebar/render-view-sidebar.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject, linkedSignal } from '@angular/core';
-import { ElementService } from '../../../wad/rendering/element.service';
+import { WadElementService } from '../../../wad/rendering/element.service';
 
 @Component({
   selector: 'app-render-view-sidebar',
   templateUrl: './render-view-sidebar.component.html',
 })
 export class RenderViewSidebarComponent {
-  elementService = inject(ElementService);
+  elementService = inject(WadElementService);
 
   elements = linkedSignal(() => this.elementService.elements());
 }
