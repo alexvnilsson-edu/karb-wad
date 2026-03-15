c7ae9ca97a66d0bbec3a8b5741c5aa943661234b
Alexander Nilsson <alexvnilsson@outlook.com>
2026-03-15 18:11:05 +0100

    chore: remove debug log

--- src/app/rendering/render-view/render-view.component.ts
+++ src/app/rendering/render-view/render-view.component.ts
@@ -103,12 +103,10 @@ export class RenderViewComponent {
   }
 
   wheel(event: WheelEvent) {
-    // Revert deltaY and
-    // determine max delta of -5 <= deltaY <= 5.
+    // Revert deltaY and determine max delta of -5 <= deltaY <= 5.
     const deltaY = -event.deltaY > 0 ? 5 : -5;
     const scale = this.renderService.scale();
     const newScale = scale + deltaY;
-    console.debug(`[wheel] delta y: ${deltaY}, old scale: ${scale}, new scale: ${newScale}`);
     this.renderService.setScale(newScale);
   }
 
